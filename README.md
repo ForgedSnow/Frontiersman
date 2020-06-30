9e16d0f85dd25ad8f89f3ffc656c02fda03ab911
TolerantSnow <mrsnowbr@gmail.com>
2020-06-30 06:46:07 -0400

    updated readme and setup

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 # CSI4930 Frontiersman
 
-A clone of Settlers of CATAN (base game, 3-4 players local multiplayer)
-The game rules are included in the directory
+A clone of Settlers of CATAN (base game, 2-4 players multiplayer)
 
-
-Make sure to install pygame
-
-py -m pip install -U pygame --user
-
-add pygame to your IDE (if using one);
-I recommend using pycharm for your IDE
-
-https://stackoverflow.com/questions/28453854/add-pygame-module-in-pycharm-ide
+Requires python 3.8 and pygame
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Frontiersman-ForgedSnow", # Replace with your own username
+    name="Frontiersman-ForgedSnow",
     version="1.0.0",
     author="Brian Snow",
     author_email="snowb@ufl.edu",
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/ForgedSnow/Frontiersman",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
